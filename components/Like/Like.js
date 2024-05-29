0daf390f98dbea15028443fbfc8d6b97c53a367a
nez1d <137664115+nez1d@users.noreply.github.com>
2024-05-29 03:51:47 +0300

    Add files via upload

---
+++ components/Like/Like.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('like-menu');
+
+function OverLikeMenu(){
+    const menu = document.getElementById('like-menu').classList.add("active");
+};
+
+function OutLikeMenu(){
+    const menu = document.getElementById('like-menu').classList.remove("active");
+};
